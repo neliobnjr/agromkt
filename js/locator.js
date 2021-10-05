f833fa518519b6d9138d7d0fbe5357921f79a5ef
Nelio Batista <nelio.batistadonasci@ndus.edu>
2021-10-05 08:49:02 -0300

    Create locator.js

---
+++ js/locator.js
@@ -0,0 +1,21 @@
+function getLocation() {   
+    console.log('getLocation was called') 
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, positionError);
+    } else {
+        hideLoadingDiv()
+        console.log('Geolocation is not supported by this device')
+    }
+}
+
+function positionError() {    
+    console.log('Geolocation is not enabled. Please enable to use this feature')
+
+    if(allowGeoRecall) getLocation()
+}
+
+function showPosition(){
+    console.log('posiiton accepted')
+    allowGeoRecall = false
+}
+
